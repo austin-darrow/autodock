bbd61dcaa60a2cd3818b81ed91ae4b48d38075e2
Austin Darrow <adarrow@utexas.edu>
2023-01-20 07:59:33 -0600

    Python Vina module--works for 1 ligand docking but not virtual screening

--- python_vina.py
+++ python_vina.py
@@ -3,10 +3,11 @@ import sys
 import os
 import time
 from os.path import basename
+from vina import Vina
 
 NUM_LIGANDS = '10'
-MODEL = sys.argv[1]
-FORCEFIELD = sys.argv[2]
+#MODEL = sys.argv[1]
+#FORCEFIELD = sys.argv[2]
 
 def benchmark(start_time, end_time, num_ligands, model, forcefield):
     benchmark_file = open("benchmarks.txt", 'a+')
@@ -37,6 +38,30 @@ def dock_ligands(model='basic', forcefield='vina'):
                             --maps ./input/maps/1fpu_receptor_rigid --scoring ad4 --exhaustiveness 32 --out \
                             ./output/ad4_flex/AD4_OUT_{file} | sed -n '22p; 37p' | awk '{{print $2}}' >> results_ad4_flex.txt"], shell=True)
 
+def vina_dock(ligand):
+    v = Vina(sf_name='vina')
+    v.set_receptor('./input/receptors/1iep_receptor.pdbqt')
+    v.set_ligand_from_file(ligand)
+    v.compute_vina_maps(center=[15.190, 53.903, 16.917], box_size=[20, 20, 20])
+    
+    # Score the current pose
+    energy = v.score()
+    print('Score before minimization: %.3f (kcal/mol)' % energy[0])
+
+    # Minimized locally the current pose
+    energy_minimized = v.optimize()
+    print('Score after minimization : %.3f (kcal/mol)' % energy_minimized[0])
+    v.write_pose(f'./output/vina_python/{ligand}_minimized.pdbqt', overwrite=True)
+    
+    # Dock the ligand
+    v.dock(exhaustiveness=32, n_poses=20)
+    v.write_poses(f'./output/vina_python/{ligand}_vina_out.pdbqt', n_poses=5, overwrite=True)
+
+def vina_dock_multiple():
+    for filename in os.listdir('./input/ligands'):
+        vina_dock(filename)
+
+
 def sort():
     INPUTFILE = f'results_{FORCEFIELD}_{MODEL}.txt'
     OUTPUTFILE = 'parsed_results.txt'
@@ -54,11 +79,12 @@ def sort():
         data.writelines(sorted(result, key=lambda x: float(x.split()[0])))
 
 def run_trial():
-    start_time = time.time()
+   # start_time = time.time()
     dock_ligands(MODEL, FORCEFIELD)
-    end_time = time.time()
-    benchmark(start_time, end_time, NUM_LIGANDS, MODEL, FORCEFIELD)
-    sort()
+   # end_time = time.time()
+   # benchmark(start_time, end_time, NUM_LIGANDS, MODEL, FORCEFIELD)
+   # sort()
    
 
-run_trial()
+#run_trial()
+vina_dock_multiple()
