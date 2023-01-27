6b596d641c0578793a80442552ba4470989f7244
Austin Darrow <adarrow@utexas.edu>
2023-01-27 15:17:04 -0600

    Bugfix on single script merger

--- autodock.py
+++ autodock.py
@@ -15,12 +15,6 @@ ligand_library = ''
 num_ligands = 10000
 model = 'basic'
 
-def benchmark(start_time, end_time, num_ligands, model, docking_type):
-    benchmark_file = open("benchmarks.txt", 'a+')
-    benchmark_file.write("\n" + f'Ligands: {num_ligands}' + "\n" + f'Model: {model}' + "\n" + f'Docking type: {docking_type}'+ "\n")
-    benchmark_file.write("Start: " + str(start_time) + " || End: " + str(end_time) + f' || Total time: {end_time - start_time}' + "\n")
-    benchmark_file.close()
-
 def sort():
     subprocess.run(["cat results* >> results_merged.txt"], shell=True)
     INPUTFILE = 'results_merged.txt'
@@ -43,35 +37,43 @@ def run_docking(ligand, v, filename):
     v.set_ligand_from_string(ligand)
     v.dock()
     v.write_poses(f'./output/{docking_type}/output_{filename}', n_poses=1, overwrite=True)
-'''
-def prep_maps(receptor):
-    if exists('{receptor}.gpf'):
-        subprocess.run([f"rm {receptor}.gpf"], shell=True)
-    subprocess.run([f"python3 /scripts/write-gpf.py --box /configs/config.config /input/receptors/{receptor}.pdbqt"], shell=True)
-    subprocess.run([f"/scripts/autogrid4 -p /input/receptors/{receptor}.gpf"], shell=True)
-'''
 
+
+def prep_maps(receptor):
+    if docking_type == 'ad4':
+        if exists('{receptor}.gpf'):
+            subprocess.run([f"rm {receptor}.gpf"], shell=True)
+        subprocess.run([f"python3 /scripts/write-gpf.py --box /configs/config.config /input/receptors/{receptor}.pdbqt"], shell=True)
+        subprocess.run([f"/scripts/autogrid4 -p /input/receptors/{receptor}.gpf"], shell=True)
+
+def conversion(receptor):
+    if exists('./input/receptors/{receptor}.pdb'):
+        subprocess.run([f'./scripts/prepare_receptor -r ./input/receptors/{receptor}.pdb -o ./input/receptors/{receptor}.pdbqt'], shell=True)
+
+def pre_processing():
+    prep_maps(receptor)
+    conversion(receptor)
 
 def main():
     # Setup
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
     rank = comm.Get_rank()
-
-    # Pre-Processing
-    start_time = time.time()
     
-    receptor = '1iep_receptor'
-    docking_type = 'vina'
-    ligands = pickle.load(open('./input/ligands_10.pkl', 'rb'))
+    # Pre-Processing (only rank 0)
+    if rank == 0:
+        pre_processing()
+    comm.Barrier()
 
     # Initialize Vina or AD4 configurations
+    docking_type = 'vina'
+    ligands = pickle.load(open('./input/ligands_10.pkl', 'rb'))
+    
     if docking_type == 'vina':
         v = Vina(sf_name='vina', cpu=0, verbosity=0)
         v.set_receptor(f'./input/receptors/{receptor}.pdbqt')
         v.compute_vina_maps(center=[15.190, 53.903, 16.917], box_size=[20, 20, 20])
     elif docking_type == 'ad4':
-        prep_maps(receptor)    
         v = Vina(sf_name='ad4', cpu=0, verbosity=0)
         v.load_maps(map_prefix_filename='1iep_receptor')
 
@@ -88,4 +90,5 @@ def main():
     sort()
     end_time = time.time()
     benchmark(start_time, end_time, num_ligands, model, docking_type)
+
 main()
