3585637d88f48372712c8564fe73fdf9976f8ec4
Austin Darrow <adarrow@utexas.edu>
2023-01-30 16:58:30 -0600

    DRY out script paths

--- autodock.py
+++ autodock.py
@@ -10,29 +10,31 @@ comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
  
-receptor='1iep_receptor'
-docking_type = 'ad4'
-ligand_library = ''
-user_configs = ['center_x = 15.190', 'center_y = 53.903', \
-                'center_z = 16.917', 'size_x = 20.0', \
-                'size_y = 20.0', 'size_z = 20.0']
+receptor='1fpu_receptor'
+receptor_path = f'/scratch/09252/adarrow/autodock/input/receptors/{receptor}'
+docking_type = 'vina'
+ligand_library = './input/ligands_10.pkl'
+config_path = '/.configs/config.config'
+user_configs = {'center_x': '15.190', 'center_y': '53.903', \
+                'center_z': '16.917', 'size_x': '20.0', \
+                'size_y': '20.0', 'size_z': '20.0'}
 
-def prep_config(user_configs):
-    with open('./configs/config.config', 'w+') as f:
-        for config in user_configs:
-            f.write(f'{config}\n')
+def prep_config():
+    with open(config_path, 'w+') as f:
+        for config, value in user_configs:
+            f.write(f'{config} = {value}\n')
     f.close()
 
-def prep_maps(receptor):
+def prep_maps():
     if docking_type == 'ad4':
         if exists(f'{receptor}.gpf'):
             subprocess.run([f"rm {receptor}.gpf"], shell=True)
-        subprocess.run([f"python3 ./scripts/write-gpf.py --box ./configs/config.config ./input/receptors/{receptor}.pdbqt"], shell=True)
+        subprocess.run([f"python3 ./scripts/write-gpf.py --box {config_path} {receptor_path}.pdbqt"], shell=True)
         subprocess.run([f"./scripts/autogrid4 -p {receptor}.gpf"], shell=True)
 
-def prep_receptor(receptor):
-    if exists(f'/scratch/09252/adarrow/autodock/input/receptors/{receptor}H.pdb'):
-        subprocess.run([f'./scripts/prepare_receptor -r ./input/receptors/{receptor}H.pdb -o ./input/receptors/{receptor}.pdbqt'], shell=True)
+def prep_receptor():
+    if exists(f'{receptor_path}H.pdb'):
+        subprocess.run([f'./scripts/prepare_receptor -r {receptor_path}H.pdb -o {receptor_path}.pdbqt'], shell=True)
 
 def run_docking(ligand, v, filename):
     v.set_ligand_from_string(ligand)
@@ -60,24 +62,25 @@ def sort():
 def main():
     # Pre-Processing (only rank 0)
     if rank == 0:
-        prep_config(user_configs)
-        prep_receptor(receptor)
-        prep_maps(receptor)
+        prep_config()
+        prep_receptor()
+        prep_maps()
         for i in range(size):
             comm.send('', dest=i)
     else:
         comm.recv(source=0)
 
     # Initialize Vina or AD4 configurations
-    ligands = pickle.load(open('./input/ligands_10.pkl', 'rb'))
+    ligands = pickle.load(open(ligand_library, 'rb'))
     
     if docking_type == 'vina':
-        v = Vina(sf_name='vina', cpu=0, verbosity=0)
-        v.set_receptor(f'./input/receptors/{receptor}.pdbqt')
-        v.compute_vina_maps(center=[15.190, 53.903, 16.917], box_size=[20, 20, 20])
+        v = Vina(sf_name='vina', cpu=1, verbosity=0)
+        v.set_receptor(f'{receptor_path}.pdbqt')
+        uc = user_configs
+        v.compute_vina_maps(center=[uc[center_x], uc[center_y], uc[center_z]], box_size=[uc[size_x], uc[size_y], uc[size_z]])
     elif docking_type == 'ad4':
-        v = Vina(sf_name='ad4', cpu=0, verbosity=0)
-        v.load_maps(map_prefix_filename='1iep_receptor')
+        v = Vina(sf_name='ad4', cpu=1, verbosity=0)
+        v.load_maps(map_prefix_filename = receptor)
 
     # Run docking
     for index, filename in enumerate(ligands):
