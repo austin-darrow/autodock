d6965b9a871d2140dcce8788eb6bf15ffadfa5eb
Austin Darrow <adarrow@utexas.edu>
2023-01-31 09:01:31 -0600

    Update ligand_pickling script to create variable-sized .pkl files

--- scripts/ligand_pickler.py
+++ scripts/ligand_pickler.py
@@ -1,15 +1,38 @@
 import os
+import os.path
 import pickle
 
+MAX_LIGANDS_PER_SET = 1000
+
 ligands = {}
-path_10 = '../../Enamine-PC/10_set'
-path_50 = '../../ZINC-in-trials/2'
-path_200 = '../../Enamine-PC/200_set'
-path_10k = '../../Enamine-PC/1'
+path = '../../Enamine-PC'
+for dirpath, dirnames, filenames in os.walk(path):
+    for filename in [f for f in filenames]:    
+        ligand = open(f'{dirpath}/{filename}', 'r')
+        ligands[filename] = ligand.read()
 
-for filename in os.listdir(path_10):
-    f = open(f'{path_10}/{filename}', 'r')
-    lines = f.read()
-    ligands[filename] = lines
+#pickle.dump(ligands, open('ligands_10.pkl', 'wb'))
 
-pickle.dump(ligands, open('ligands_10.pkl', 'wb'))
+import copy
+def split_dict_to_multiple(input_dict, max_limit=200):
+    """Splits dict into multiple dicts with given maximum size. 
+    Returns a list of dictionaries."""
+    chunks = []
+    curr_dict ={}
+    for k, v in input_dict.items():
+        if len(curr_dict.keys()) < max_limit:
+            curr_dict.update({k: v})
+        else:
+            chunks.append(copy.deepcopy(curr_dict))
+            curr_dict = {k: v}
+    # update last curr_dict
+    chunks.append(curr_dict)
+    return chunks
+
+chunked_list_of_dicts = split_dict_to_multiple(ligands, MAX_LIGANDS_PER_SET)
+
+def pickler(chunked_list_of_dicts):
+    for index, dictionary in enumerate(chunked_list_of_dicts):
+        pickle.dump(dictionary, open(f'./Enamine-PC-Pickled/ligands_{index}.pkl', 'wb'))
+
+pickler(chunked_list_of_dicts)
