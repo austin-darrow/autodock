ef9f22d5d432fb8df322b9b7da7f12fe092a6132
Austin Darrow <adarrow@utexas.edu>
2023-02-02 17:31:39 -0600

    Extend documentation within autodock.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This application runs AutoDock Vina virtual screening using the user's protein receptor and a selected ligand library on the Texas Advanced Computing Center (TACC) Lonestar6 supercomputer. Utilizing Python's mpi4py library, the application allows the user to submit batch jobs that dock up to thousands of molecules in parallel.
+This application runs AutoDock Vina virtual screening using the user's protein receptor and a selected ligand library on the [Texas Advanced Computing Center (TACC)](https://www.tacc.utexas.edu/) [Lonestar6 supercomputer](https://www.tacc.utexas.edu/systems/lonestar6). Utilizing Python's [mpi4py](https://mpi4py.readthedocs.io/en/stable/) library, the application allows the user to submit batch jobs that dock up to thousands of molecules in parallel.
 
 Note: Computer docking and virtual screening are inexact, but potentially very valuable, tools. This site is intended to provide easy access to researchers wishing to perform small numbers of docking or virtual screening experiments, but who do not have the necessary computer resources and/or computational biology backgrounds. This interface can handle most protein-ligand docking experiments, but there will always be systems where this simple interface will fail. For those cases, researchers will need to develop the necessary expertise or collaborations to perform the experiment.
 
@@ -28,7 +28,7 @@ __autodock__
  Running the application:
 -----------------------
 
-To run the application, the user must submit a job using the TACC API (TAPIS) Portal system. The following inputs are allowed:
+To run the application, the user must submit a job using the [TACC API (TAPIS) Portal system](). The following inputs are allowed:
 - __Protein Receptor__: The user _must_ upload a .pdb- or .pdbqt-formatted protein receptor file
 - __Grid Center__: The user _may_ specify X-Y-Z coordinates; default is x: 15.190, y: 53.903, z: 16.917
 - __Box Size__: The user _may_ specify X-Y-Z size limits; default is 20-20-20
@@ -47,3 +47,5 @@ To run the application, the user must submit a job using the TACC API (TAPIS) Po
 [AutodockTools](http://autodock.scripps.edu/resources/adt/index_html/), written by Drs. G. Morris and A. Olson at the Scripps Institute is used to convert proteins and ligands to the format required by AutoDock Vina. This open-source program has been made freely available by the authors.
 
 [1] "AutoDock Vina: Improving the Speed and Accuracy of Docking with a New Scoring Function, Efficient Optimization and Multithreading", (2010) O. Trott and A. J. Olson, J. Comp. Chem. 31, 455-461.
+
+
--- autodock.py
+++ autodock.py
@@ -1,18 +1,21 @@
 from vina import Vina
 from mpi4py import MPI
 import subprocess
-import pickle
+import pickle # For unpickling ligand files
 import os
-import blosc
+import blosc # For decompressing compressed ligand files
 from os.path import exists
-from os.path import basename
-from sys import argv
+from os.path import basename # Used in the sorting function
+import argparse # To accept user inputs as command line arguments
 
-# Setup
+# Setup base MPI declarations
+# In this MPI implementation, rank 0 acts as the director that all other ranks work for. Rank 0 first completes all pre-processing. As a final step, it creates a list containing full filepaths to all ligand files in the ligand library. Then it accepts messages from worker ranks saying they are ready for more work. It sends work for as long as there are ligand files left. Then rank 0 waits to hear from all ranks that they have finished processing, then proceeds to do the post-processing work. 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
+
+# Assign user inputs
 receptor='1fpu_receptor'
 flex_receptor=f'{receptor}_flex'
 receptor_path = f'/scratch/09252/adarrow/autodock/input/receptors'
@@ -25,25 +28,32 @@ user_configs = {'center_x': '15.190', 'center_y': '53.903', \
                 'center_z': '16.917', 'size_x': '20.0', \
                 'size_y': '20.0', 'size_z': '20.0'}
 
+
 def prep_config():
+# Write user inputted configurations (grid center and box size) to a config file for AutoDock Vina to use
     with open(config_path, 'w+') as f:
         for config, value in user_configs.items():
             f.write(f'{config} = {value}\n')
     f.close()
 
+
 def prep_maps():
+# Generate affinity maps using write-gpf.py and AFDRSuite's autogrid4. Generates maps for all possible ligand atom types for a given receptor
     if docking_type == 'ad4':
         if exists(f'{receptor}.gpf'):
             subprocess.run([f"rm {receptor}.gpf"], shell=True)
         subprocess.run([f"python3 ./scripts/write-gpf.py --box {config_path} {receptor_path}/{receptor}.pdbqt"], shell=True)
         subprocess.run([f"./scripts/autogrid4 -p {receptor}.gpf"], shell=True)
 
+
 def prep_receptor():
+# Converts a PDB receptor to a PDBQT, if needed. If the user has specified flexible docking, also prepares the rigid receptor and user-chosen flexible sidechains.
     if exists(f'{receptor_path}/{receptor}H.pdb'):
         subprocess.run([f'./scripts/prepare_receptor -r {receptor_path}/{receptor}H.pdb -o {receptor_path}/{receptor}.pdbqt'], shell=True)
     if flexible == True:
         subprocess.run([f"./scripts/pythonsh ./scripts/prepare_flexreceptor.py -g {receptor}.pdbqt -r {receptor_path}/{receptor}.pdbqt -s {'_'.join(sidechains)}"], shell=True)
         subprocess.run([f"mv *receptor* {receptor_path}"], shell=True)
+
 
 def prep_ligands():
     # Returns a list where each item is the path to a pickled and compressed text file containing multiple ligand strings
@@ -53,7 +63,9 @@ def prep_ligands():
             ligand_paths.append(f'{dirpath}/{filename}')
     return ligand_paths
 
+
 def run_docking(ligands, v):
+    # Runs AutoDock on each ligand in the given set; outputs a .pdbqt file showing the pose and all scores; appends the ligand name (filename) and it's best pose/score to a temporary results file
     for index, filename in enumerate(ligands):
         ligand = ligands[filename]
         v.set_ligand_from_string(ligand)
@@ -63,22 +75,30 @@ def run_docking(ligands, v):
                         | awk '{{print $4}}' >> results_{rank}.txt; echo {filename} \
                         >> results_{rank}.txt"], shell=True)
 
+
 def unpickle_and_decompress(path_to_file):
+    # Given a filepath, decompresses and unpickles the file. Returns the contents of the file as a dictionary where keys are ligand filenames and values are the actual ligand strings
     with open(path_to_file, 'rb') as f:
         compressed_pickle = f.read()
     depressed_pickle = blosc.decompress(compressed_pickle)
     dictionary_of_ligands = pickle.loads(depressed_pickle)
     return dictionary_of_ligands
 
+
 def pre_processing():
+    # Helper function to reduce clutter in main()
     prep_config()
     prep_receptor()
     prep_maps()
     ligands = prep_ligands()
     return ligands
 
+
 def processing():
-    # Initialize Vina or AD4 configurations
+    # Long method; difficult to de-couple any one part without breaking many things
+
+    # Initialize docking configurations
+    # Note: Internal benchmarks showed that on a given node with 128 cores, setting ibrun -np to 32 and, below, setting CPU=4 granted the fastest docking. Verbosity set to 0 to increase speeds, as stdout cannot be captured from Vina's Python module.
     if docking_type == 'vina':
         v = Vina(sf_name='vina', cpu=4, verbosity=0)
         if flexible == True:
@@ -99,14 +119,13 @@ def processing():
         if ligand_set_path == 'no more ligands':
             comm.send('message received--proceed to post-processing',dest=0)
             break
-        # Pickle load ligand set
         ligands = unpickle_and_decompress(ligand_set_path)
-        # Dock each ligand in the set
         run_docking(ligands, v)
 
 
 
 def sort():
+    # Cats all results files into one, arranges each line to read: (ligand, top score), then sorts by score so that highest scoring ligands are on top; prints these sorted results are written to processed_results.txt; finally cleans up the directory
     subprocess.run(["cat results* >> results_merged.txt"], shell=True)
     INPUTFILE = 'results_merged.txt'
     OUTPUTFILE = './output/processed_results.txt'
